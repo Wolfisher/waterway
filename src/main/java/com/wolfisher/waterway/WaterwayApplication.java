b63a91f99ec3568a6fc192ed126e46a63205f869
shawnli <shawnlisyr@gmail.com>
2024-08-29 20:38:01 -0700

    project init

---
+++ src/main/java/com/wolfisher/waterway/WaterwayApplication.java
@@ -0,0 +1,13 @@
+package com.wolfisher.waterway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolfisher/waterway/WaterwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfisher.waterway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
